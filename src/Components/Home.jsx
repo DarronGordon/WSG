52d9f5d01e45ca22738b40f454c29d04c563c329
Dax <darronmgordon@gmail.com>
2022-12-29 10:58:47 +0200

    firstCommit

---
+++ src/Components/Home.jsx
@@ -0,0 +1,25 @@
+
+
+
+export const HomePage =() =>{
+
+    return(
+        <div className="homeContainer">
+        <div className="homeHeader">
+
+        </div>
+        <div className="homeSlider">
+
+        </div>
+        <div className="homeAboutGym">
+
+        </div>
+        <div className="homeAboutInstructors">
+
+        </div>
+        <div className="homeTestamonials">
+
+        </div>
+        </div>
+    )
+} 
