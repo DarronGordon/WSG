5dc432e209561984fb4f2e0dcf063956a2434888
Darron <darronmgordon@gmail.com>
2023-01-02 07:56:02 +0200

    time table start

--- src/Components/TimeTablePg.jsx
+++ src/Components/TimeTablePg.jsx
@@ -4,7 +4,27 @@ export const TimeTablePg = () =>
 
     return(
         <div>
-            <h1>TimeTable</h1>
+            <h1>TimeTable</h1>  
+            <div className="timeTableContainer">
+            <div className="monday">
+            <div className="time">
+        
+            </div>
+            <div className="discription">
+
+            </div>
+
+            </div>
+              {/* <ul className="timeTable">
+              <ul className="monday">
+                <li>item1</li>
+              </ul>
+              <ul>
+                <li>item1</li>
+              </ul>
+
+              </ul> */}
+            </div>
         </div>
     )
 }
